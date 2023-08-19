59a77ffe3a1ccec2b845b9e5266ab59530779f50
Inno Paolo <102833497+innopaolo@users.noreply.github.com>
2023-08-19 20:23:25 +0800

    Create module to test game flow

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ const gameboard = (() => {
     console.log(_cells);
 })();
 
+// Module pattern for gameController
+const gameController = ((p1, p2) => {
+    let currentPlayer = null;
+
+    const startGame = () => {
+        currentPlayer = p1;
+        console.log(`Game start. ${p1.name} goes first.`);
+    };
+    const switchPlayer = () => {
+        currentPlayer = (currentPlayer == p1) ? p2 : p1;
+        console.log(`Switched to ${p2.name}'s turn.`)
+    };
+    const  getCurrentPlayer = () => currentPlayer;
+    
+    return { startGame, switchPlayer, getCurrentPlayer };
+})(player1, player2);
+
+gameController.startGame();
+gameController.switchPlayer(player1, player2);
+console.log(gameController.getCurrentPlayer());
