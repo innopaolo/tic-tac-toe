632385820311a972c865ec2b3c210f6b71f4ac47
Inno Paolo <102833497+innopaolo@users.noreply.github.com>
2023-08-21 12:30:11 +0800

    Create player name fields when symbols are clicked

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-// Factory function for players
+// Factory function for players and initialize two players
 const createPlayer = (name, symbol, points) => {
     return { name, symbol, points };
 };
 
-const player1 = createPlayer("Player 1", "X", 0);
-const player2 = createPlayer("Player 2", "O", 0);
+let player1 = createPlayer("Player 1", "X", 0);
+let player2 = createPlayer("Player 2", "O", 0);
+
+
+// Global variables and DOM manipulation
+const numberOfWins = 3;
+const player1Element = document.querySelector("#player1");
+const player2Element = document.querySelector("#player2");
+
+player1Element.addEventListener("click", () => {
+    const input = document.querySelector(".hidden-content input");
+    input.style.display = "block";
+    input.focus();
+});
+
+player2Element.addEventListener("click", () => {
+    const input = document.querySelector("#hidden-input2");
+    input.style.display = "block";
+    input.focus();
+    console.log(input);
+});
+
 
 
 
@@ -12,22 +32,25 @@ const player2 = createPlayer("Player 2", "O", 0);
 const gameBoard = (() => {
     const _cells = [null, null, null, null, null, null, null, null, null];
 
-    const winningCombinations = [
+    const _winningCombinations = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
         [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
         [0, 4, 8], [2, 4, 6]             // Diagonals
     ];
 
     const _checkWinner = (symbol) => {
-        return winningCombinations.some(combination => {
+        return _winningCombinations.some(combination => {
             return combination.every(index => _cells[index] === symbol);
         });
     };
+
     const _ultimateVictoryCondition = (player) => {
-        if (player.points === 3) {
+        if (player.points === numberOfWins) {
             console.log(`${player.name} has !!Ultimate Victory!!`);
         }
     };
+
+    // Checks if every cell does not hold the value null
     const _checkForTie = () => {
         if (_cells.every(cell => cell !== null )) {
             return true;
@@ -35,6 +58,7 @@ const gameBoard = (() => {
             return false;
         }
     }; 
+
     const setCells = (index, player) => {
         if (_cells[index] === null) {
             _cells[index] = player.symbol;
@@ -53,9 +77,11 @@ const gameBoard = (() => {
             console.log("It's a tie!");
         }
     };
+
     const showCells = () => {
         console.log(_cells);
     };
+
     const reset = () => {
         _cells.forEach((element, index, array) => {
             array[index] = null;
@@ -78,38 +104,39 @@ const gameController = ((p1, p2) => {
         _currentPlayer = p1;
         console.log(`Game start. ${p1.name} goes first.`);
     };
+
     const setPlayerMove = (index) => {
         console.log(`${_currentPlayer.name} marking his square...`)
         gameBoard.setCells(index, _currentPlayer);
     };
+
     const switchPlayer = () => {
         _currentPlayer = (_currentPlayer == p1) ? p2 : p1;
         console.log(`Switched to ${_currentPlayer.name}'s turn.`)
     };
+
     const getCurrentPlayer = () => _currentPlayer;
     
     return { startGame, switchPlayer, getCurrentPlayer, setPlayerMove };
 })(player1, player2);
 
-gameController.startGame();
-gameController.setPlayerMove(0);
-gameBoard.showCells();
-gameController.switchPlayer();
-gameController.setPlayerMove(2);
-gameBoard.showCells();
-gameController.switchPlayer();
-gameController.setPlayerMove(1);
-gameBoard.showCells();
-gameController.switchPlayer();
-gameController.setPlayerMove(4);
-gameBoard.showCells();
-gameController.switchPlayer();
-gameController.setPlayerMove(7);
-gameBoard.showCells();
-gameController.switchPlayer();
-gameController.setPlayerMove(6);
-gameBoard.showCells();
+// gameController.startGame();
+// gameController.setPlayerMove(0);
+// gameBoard.showCells();
+// gameController.switchPlayer();
+// gameController.setPlayerMove(2);
+// gameBoard.showCells();
+// gameController.switchPlayer();
+// gameController.setPlayerMove(1);
+// gameBoard.showCells();
+// gameController.switchPlayer();
+// gameController.setPlayerMove(4);
+// gameBoard.showCells();
+// gameController.switchPlayer();
+// gameController.setPlayerMove(7);
+// gameBoard.showCells();
+// gameController.switchPlayer();
+// gameController.setPlayerMove(6);
+// gameBoard.showCells();
 
-gameBoard.reset();
-
-
+// gameBoard.reset();
